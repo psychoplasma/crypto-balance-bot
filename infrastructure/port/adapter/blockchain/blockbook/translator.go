e333b9d4f460b7590ef31d129278f0283953dfa2
Mustafa Morca <psychoplasma@gmail.com>
2021-02-11 14:49:22 +0900

    Remove commented out snippets

--- infrastructure/port/adapter/blockchain/blockbook/translator.go
+++ infrastructure/port/adapter/blockchain/blockbook/translator.go
@@ -16,16 +16,7 @@ func (tr BitcoinTranslator) ToAccountMovements(address string, v interface{}) (*
 	txs, _ := v.([]Transaction)
 	am := domain.NewAccountMovements(address)
 
-	// blocks := make(map[int]string)
-
 	for _, tx := range txs {
-		// if blocks[tx.BlockHeight] == "" {
-		// 	fmt.Printf("block #%d ..%s..\n", tx.BlockHeight, tx.BlockHash)
-		// 	blocks[tx.BlockHeight] = tx.BlockHash
-		// } else if blocks[tx.BlockHeight] != tx.BlockHash {
-		// 	fmt.Printf("***block #%d ..%s..\n", tx.BlockHeight, tx.BlockHash)
-		// }
-
 		// Inputs will be reflected as a spent
 		for _, in := range tx.Inputs {
 			if in.Addresses[0] != address {
@@ -37,8 +28,6 @@ func (tr BitcoinTranslator) ToAccountMovements(address string, v interface{}) (*
 				return nil, fmt.Errorf("bitcoin translation error, cannot convert in.Value(%s) to bigint", in.Value)
 			}
 			am.SpendBalance(tx.BlockHeight, tx.TxID, val)
-
-			// fmt.Printf("\t-%s in %s..\n", val.String(), tx.TxID[0:7])
 		}
 
 		// Outputs will be reflected as a receive
@@ -52,8 +41,6 @@ func (tr BitcoinTranslator) ToAccountMovements(address string, v interface{}) (*
 				return nil, fmt.Errorf("bitcoin translation error, cannot convert out.Value(%s) to bigint", out.Value)
 			}
 			am.ReceiveBalance(tx.BlockHeight, tx.TxID, val)
-
-			// fmt.Printf("\t+%s in %s..\n", val.String(), tx.TxID[0:7])
 		}
 	}
 
