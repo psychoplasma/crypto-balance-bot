14f4ba5056c91fb093883f41cf0610b480d37bb5
Mustafa Morca <psychoplasma@gmail.com>
2023-06-23 16:36:56 +0900

    Add headers to GET requests

--- infrastructure/net/http.go
+++ infrastructure/net/http.go
@@ -8,7 +8,15 @@ import (
 
 // GetJSON makes a HTTP GET call and expects a JSON response
 func GetJSON(url string, v interface{}) error {
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0`)
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
