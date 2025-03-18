7e5a5d292a90aba1671bd83b22778f03b8c8e55c
Mustafa Morca <psychoplasma@gmail.com>
2025-03-18 14:29:09 +0900

    Workaround bigint serialization issue

--- web-app/backend/src/blockchain/blockchain.service.ts
+++ web-app/backend/src/blockchain/blockchain.service.ts
@@ -1,52 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClient, Subscription } from '@prisma/client';
 
 @Injectable()
 export class BlockchainService {
-    constructor(private readonly prismaClient: PrismaClient) {}
+  constructor(private readonly prismaClient: PrismaClient) {}
 
-    async getSubscriptionsByUserId(userId: string): Promise<Subscription[]> {
-        return this.prismaClient.subscription.findMany({ where: { userId } });
+  async getSubscriptionsByUserId(userId: string): Promise<Subscription[]> {
+    return await this.prismaClient.subscription.findMany({ where: { userId } });
+  }
+
+  async getSubscriptionsByUserIdAndCurreny(userId: string, currency: string): Promise<Subscription[]> {
+    return this.prismaClient.subscription.findMany({
+      where: { userId, currency },
+    });
+  }
+
+  async subscribe(
+    userId: string,
+    currency: string,
+    address: string,
+    blockHeight: number,
+    startingBlockHeight: number,
+  ): Promise<Subscription> {
+    const user = await this.prismaClient.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
     }
 
-    async getSubscriptionsByUserIdAndCurreny(userId: string, currency: string): Promise<Subscription[]> {
-        return this.prismaClient.subscription.findMany({ where: { userId, currency } });
+    return await this.prismaClient.subscription.create({ data: {
+      userId,
+      currency,
+      account: address,
+      blockHeight,
+      startingBlockHeight,
+      totalReceived: 0,
+      totalSpent: 0,
+      filters: '',
+    }});
+  }
+
+  async unsubscribe(userId: string, currency: string, account: string): Promise<void> {
+    const user = await this.prismaClient.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
     }
 
-    async subscribe(
-        userId: string,
-        currency: string,
-        address: string,
-        blockHeight: number,
-        startingBlockHeight: number,
-    ): Promise<Subscription> {
-        // TODO: use logged-in user instead
-        await this.prismaClient.user.findUniqueOrThrow({ where: { id: userId } });
+    const subscription = await this.prismaClient.subscription.findUnique({
+      where: { userId_currency_account: { userId, account, currency } },
+    });
 
-        return await this.prismaClient.subscription.create({ data: {
-            userId,
-            currency,
-            account: address,
-            blockHeight,
-            startingBlockHeight,
-            totalReceived: 0,
-            totalSpent: 0,
-            filters: '',
-        }});
+    if (!subscription) {
+      throw new NotFoundException(`Subscription not found`);
     }
 
-    async unsubscribe(userId: string, currency: string, account: string): Promise<void> {
-        // TODO: use logged-in user instead
-        await this.prismaClient.user.findUniqueOrThrow({ where: { id: userId } });
-
-        const subscription = await this.prismaClient.subscription.findUniqueOrThrow(
-            { where: {
-                userId_currency_account: { userId, account, currency }}
-             },
-        );
-
-        await this.prismaClient.subscription.delete({
-            where: { id: subscription.id, userId, account, currency },
-        });
-    }
+    await this.prismaClient.subscription.delete({
+      where: { id: subscription.id, userId, account, currency },
+    });
+  }
 }
--- web-app/backend/src/main.ts
+++ web-app/backend/src/main.ts
@@ -1,6 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare global {
+  interface BigInt {
+    toJSON(): string;
+  }
+}
+
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
